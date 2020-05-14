e448b9854ef5da46dd86f667f59366e509060779
azabraao <az.abraao@gmail.com>
2020-05-13 21:40:23 -0300

    Add outside-layout component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,12 +4,14 @@ import { InsideHeaderComponent } from "./inside-header/inside-header.component";
 import { InsideFooterComponent } from "./inside-footer/inside-footer.component";
 import { InsideLayoutComponent } from "./inside-layout/inside-layout.component";
 import { RouterModule } from "@angular/router";
+import { OutsideLayoutComponent } from './outside-layout/outside-layout.component';
 
 @NgModule({
   declarations: [
     InsideHeaderComponent,
     InsideFooterComponent,
     InsideLayoutComponent,
+    OutsideLayoutComponent,
   ],
   imports: [CommonModule, RouterModule],
 })
